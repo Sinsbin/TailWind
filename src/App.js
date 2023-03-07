2a1eec17d16654014472dce7b0abeee1e718bfb9
Bin <k1234988@gmail.com>
2023-03-07 10:02:24 +0800

    done

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
     
     <body>
       <div class=" m-8 h-32 w-32 border border-gray-500 p-8 scale-80 md:transform-none">YO</div>
-      <div class="bg-verybeautiful w-14 text-Redforfire text-center" >WTF</div>
+      <div class="bg-verybeautiful w-14 text-Redforfire text-center" >Text</div>
       <div class="rounded bg-sky-400 hover:bg-sky-700 w-32 text-center text-white italic">Save Changes</div>
       <div class="flex justify-center space-x-2">
         <button type="button" class="bg-Mypurple w-32 rounded ... focus:outline-none focus:bg-Mypurple focus:ring-4 focus:ring-orange ">Button</button>
       </div>
-    <div clss="flex justify-center space-x-2">
-      <button type="button" class=" text-center w-20 bg-orange  sm:bg-green  md:bg-yellow  lg:bg-sky-500 ">text here</button>
-    </div>
+      <div class=" flex items-center justify-center">
+      <button type="button" class="text-center w-20 bg-orange  sm:bg-green  md:bg-yellow  lg:bg-sky-500 ">text here</button>
+      </div>
 </body>
   );
 }
